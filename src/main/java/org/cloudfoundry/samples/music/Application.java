88b2113fe6ea1f93ba6d71d26c3bf4ed3a208065
Karsten Schnitter <k.schnitter@sap.com>
2022-10-04 18:47:25 +0200

    Log OTel Trace and Span
    Register a servlet filter to log the current trace and span id at request start.
    If no W3C header is found, this is nulled for io.opentelemetry.api:1.18.0.
    
    Signed-off-by: Karsten Schnitter <k.schnitter@sap.com>

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -1,9 +1,22 @@
 package org.cloudfoundry.samples.music;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer;
 import org.cloudfoundry.samples.music.repositories.AlbumRepositoryPopulator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
@@ -14,5 +27,27 @@ public class Application {
                 .listeners(new AlbumRepositoryPopulator())
                 .application()
                 .run(args);
+    }
+
+    @Bean
+    public FilterRegistrationBean logCurrentSpanContext() {
+
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setName("span-logging");
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST);
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        filterRegistrationBean.setFilter(new HttpFilter() {
+
+            private final Logger LOG = LoggerFactory.getLogger("span-logging");
+
+            @Override
+            protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+                SpanContext spanContext = Span.current().getSpanContext();
+                LOG.info("Request to {} with trace id {} and span id {}.", request.getRequestURI(), spanContext.getTraceId(), spanContext.getSpanId());
+                chain.doFilter(request, response);
+            }
+        });
+        return filterRegistrationBean;
     }
 }
